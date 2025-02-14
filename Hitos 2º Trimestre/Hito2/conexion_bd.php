6784deeddc7d23e37c40b01a466b7fc61422c146
Pache0678 <ivan.pachecoperez24@campusfp.es>
2025-02-14 14:20:36 +0100

    Add files via upload

---
+++ Hitos 2º Trimestre/Hito2/conexion_bd.php
@@ -0,0 +1,4 @@
+<?php
+$conexion = new mysqli("localhost", "root", "curso", "gestion_tareas"); // Establece una conexión a la base de datos MySQL.
+$conexion->set_charset("utf8"); // Configura el conjunto de caracteres a UTF-8 para manejar correctamente caracteres especiales y diferentes idiomas.
+?>
---
+++ Hitos 2º Trimestre/Hito2/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: Login/login.php"); // Redirige al usuario a la página de inicio de sesión (login.php).
+exit(); // Finaliza el script para asegurarse de que no se ejecute ningún código adicional después de la redirección.
+?>
